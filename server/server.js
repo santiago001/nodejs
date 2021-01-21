4c38aa716fec07fdf450471c6aaaff72dd1370f3
Santiago Martinez <fmartineza@corferias.com>
2021-01-21 11:36:20 -0500

    Tres

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
-console.log("Hola Santiago Pruebas Dos");
+const http = require('http');
+const colors = require('colors');
+const handleServer = function( req, res){
+    res.writeHead(200, { 'Content-type': 'text/html' } );
+    res.write('<h1> Hola mundo desde NodeJs </h1>');
+    res.end();
+    console.log("Realizada petición");
+}
+
+const server = http.createServer(handleServer);
+
+server.listen(3000, function(){
+    console.log('Server on port 3000'.green);
+})
