645297a357cc3a6d0b437a61ea1a553c688a65fa
Santiago Martinez <fmartineza@corferias.com>
2021-01-25 21:20:14 -0500

    Pruebas

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const http = require('http');
 const colors = require('colors');
+let nombre = process.env.Nombre || "Santiago Martinez";
 const handleServer = function( req, res){
     res.writeHead(200, { 'Content-type': 'text/html' } );
-    res.write('<h1> Hola mundo desde NodeJs </h1>');
+    res.write('<h1> Hola mundo '+ nombre + '</h1>');
     res.end();
     console.log("Realizada petición");
 }
